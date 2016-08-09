cb9cc923a2284e8e8a021cc86d476f2fc733c38e
Sebastian Kroczek <sk@xbug.de>
2016-08-09 13:43:28 +0200

    Limit each lable to min one and max 63 octets and the domain to max 255 octests (including seperator) (see RFC1034 and RFC2181)

--- src/Punycode.php
+++ src/Punycode.php
@@ -1,6 +1,9 @@
 <?php
 namespace TrueBV;
 
+use TrueBV\Exception\DomainOutOfBoundsException;
+use TrueBV\Exception\LabelOutOfBoundsException;
+
 /**
  * Punycode implementation as described in RFC 3492
  *
@@ -76,10 +79,16 @@ class Punycode
         $input = mb_strtolower($input, $this->encoding);
         $parts = explode('.', $input);
         foreach ($parts as &$part) {
+            if (($length = strlen($part)) < 1) {
+                throw new LabelOutOfBoundsException(sprintf('The length of any one label is limited to between 1 and 63 octets, but %s given.', $length));
+            }
             $part = $this->encodePart($part);
         }
+        if (($length = strlen($output = implode('.', $parts))) > 255) {
+            throw new DomainOutOfBoundsException(sprintf('A full domain name is limited to 255 octets (including the separators), %s given.', $length));
+        }
 
-        return implode('.', $parts);
+        return $output;
     }
 
     /**
@@ -146,8 +155,11 @@ class Punycode
             $delta++;
             $n++;
         }
+        if (($length = strlen($output = static::PREFIX.$output)) > 63 || $length < 1) {
+            throw new LabelOutOfBoundsException(sprintf('The length of any one label is limited to between 1 and 63 octets, but %s given.', $length));
+        }
 
-        return static::PREFIX . $output;
+        return $output;
     }
 
     /**
@@ -161,6 +173,9 @@ class Punycode
         $input = strtolower($input);
         $parts = explode('.', $input);
         foreach ($parts as &$part) {
+            if (($length = strlen($part)) > 63 || $length < 1) {
+                throw new LabelOutOfBoundsException(sprintf('The length of any one label is limited to between 1 and 63 octets, but %s given.', $length));
+            }
             if (strpos($part, static::PREFIX) !== 0) {
                 continue;
             }
@@ -168,8 +183,11 @@ class Punycode
             $part = substr($part, strlen(static::PREFIX));
             $part = $this->decodePart($part);
         }
+        if (($length = strlen($output = implode('.', $parts))) > 255) {
+            throw new DomainOutOfBoundsException(sprintf('A full domain name is limited to 255 octets (including the separators), %s given.', $length));
+        }
 
-        return implode('.', $parts);
+        return $output;
     }
 
     /**
